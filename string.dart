edcaec8154fecf3e1e8faa738787560150db539f
Ata <agastyo2004@gmail.com>
2023-06-10 22:30:33 +0700

    string

--- string.dart
+++ string.dart
@@ -1,5 +1,5 @@
 void main() {
-  String name = "Alex";
+  String name = " Alex ";
   String animal = "cow, sheep, camel";
   const age = 18;
 
@@ -22,4 +22,36 @@ void main() {
   print(name.substring(0, 3));
   // 0 -> start (enter)
   // 3 -> end (not entered)
+
+  // show length variable or string
+  print(name.length);
+
+  // remove space in front and back
+  print(name.trim());
+
+  // remove space in front
+  print(name.trimRight());
+
+  // remove space in back
+  print(name.trimLeft());
+
+  // get decimal value from ASCII
+  print(name.codeUnitAt(1));
+
+  // show index character from string
+  print(name.indexOf("A"));
+
+  // check whether it begins with a certain character or letter
+  print(name.startsWith(" A"));
+
+  // check whether it ends with a certain character or letter
+  print(name.endsWith(' '));
+
+  const blank = "";
+
+  // check whether string empty
+  print(blank.isEmpty);
+
+  // check wether string not empty
+  print(blank.isNotEmpty);
 }
