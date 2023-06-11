f38c58b41673068fd5c552c98c70ef57213eae97
Ata <agastyo2004@gmail.com>
2023-06-11 17:40:26 +0700

    switch case

---
+++ switchCase.dart
@@ -0,0 +1,29 @@
+void main() {
+  String day = 'Monday';
+
+  switch (day) {
+    case 'Monday':
+      print('Hari ini adalah Senin');
+      break;
+    case 'Tuesday':
+      print('Hari ini adalah Selasa');
+      break;
+    case 'Wednesday':
+      print('Hari ini adalah Rabu');
+      break;
+    case 'Thursday':
+      print('Hari ini adalah Kamis');
+      break;
+    case 'Friday':
+      print('Hari ini adalah Jumat');
+      break;
+    case 'Saturday':
+      print('Hari ini adalah Sabtu');
+      break;
+    case 'Sunday':
+      print('Hari ini adalah Minggu');
+      break;
+    default:
+      print('Hari yang dimasukkan tidak valid');
+  }
+}
