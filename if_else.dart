529687ba3e28fa523e55e608df0e57e1c707d20f
Ata <agastyo2004@gmail.com>
2023-06-11 16:52:41 +0700

    if condition

---
+++ if_else.dart
@@ -0,0 +1,17 @@
+void main() {
+  num age = 2;
+  if (age == 0) {
+    print("nol");
+  } else if (age % 2 == 0) {
+    print("Genap");
+  } else {
+    print("Ganjil");
+  }
+
+  String name = "ata";
+  if (name == 'ata') {
+    print(name);
+  } else {
+    return;
+  }
+}
