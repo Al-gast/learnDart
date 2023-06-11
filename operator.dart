0d2a8ed6aa1e8e489f20011529e13e4091ae8407
Ata <agastyo2004@gmail.com>
2023-06-11 15:31:50 +0700

    operator

---
+++ operator.dart
@@ -0,0 +1,63 @@
+void main() {
+  int a = 4;
+  int b = 10;
+  int c = a + b;
+
+  // Arithmetic Operator
+
+  // addition
+  int addition = a + b;
+  print(addition);
+
+  // subtraction
+  int subtraction = a - b;
+  print(subtraction);
+
+  // multiplication
+  int multiplication = a * b;
+  print(multiplication);
+
+  // division
+  num division = a / b;
+  print(division);
+
+  // modulo
+  num modulo = a % b;
+  print(modulo);
+
+  // equality and relational operators
+
+  // more than
+  print(a > b);
+
+  // less than
+  print(a < b);
+
+  // less than
+  print(a < b);
+
+  // equals
+  print(a == b);
+
+  // not equal
+  print(a != b);
+
+  // more than or equal
+  print(a >= b);
+
+  // lees then or equal
+  print(a <= b);
+
+  // Logical Operators
+  bool x = false;
+  bool y = true;
+
+  // && -> AND
+  print(x && y);
+
+  // || -> OR
+  print(x || y);
+
+  // ! -> NOT
+  print(!x);
+}
